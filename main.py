febe34b35dcecea46348c2b2a546eb3fab4dc52f
DegenTekle <129045510+DegenTekle@users.noreply.github.com>
2023-04-16 16:46:05 +0000

    Added dynamic data and API route

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+GAMES = [{
+  'id': 1,
+  'title': "Player 1 has won",
+  'score': '3-0'
+}, {
+  'id': 2,
+  'title': "Player 1 has won",
+  'score': '2-1'
+}]
+SCORES = [{'David': 300}, {'Greg': 450}, {'Marie': 980}]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', games=GAMES, scores=SCORES)
+
+
+@app.route("/api/SCORES")
+def list_jobs():
+  return jsonify(SCORES)
 
 
 if __name__ == "__main__":
